e5366465f6e246a58ae3b79995a2f9a5bfacb69b
Haohaoscreamandrun <j842671395@gmail.com>
2023-05-18 21:05:09 +0800

    feat: connect to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 //Include packages
 const express = require('express')
-
+const mongoose = require('mongoose')
 //variables
 const app = express()
 const port = 3000
+
+//MongoDB connection
+if(process.env.NODE_ENV !== 'production'){ //use dotenv only in development, to get .env
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection //learn the connection state
+db.on('error',()=>{
+  console.log('mongodb error!')
+})
+db.once('open',()=>{
+  console.log('mongodb connected!')
+})
+
 
 //Home route
 app.get('/', (req,res)=>{
